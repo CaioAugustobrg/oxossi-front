fab7605daabf82fbe9355b13178ca8ef8a8b2f41
Caio Braga <caioaugustobrg@gmail.com>
2023-11-06 01:32:13 -0300

    :lipstick: feat: styles

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -3,20 +3,20 @@ import styled from "styled-components";
 export const Container = styled.div`
 box-sizing: border-box;
 display: flex;
-
+width: 100%;
 overflow-x: hidden;
 flex-direction: column;
 justify-content: space-between;
-max-width: 1550px;
+/* max-width: 1550px; */
 margin: 0 auto;
 min-height: 100%;
-padding: 0px 24px;
+padding: 0px 250px;
 div.input-container {
     width: 100%;    
     margin-top: 16px;
   }
 
-  button.btn-default {
+  /* button.btn-default {
     margin-top: 24px;
     align-items: center;
     justify-content: center;
@@ -26,7 +26,7 @@ div.input-container {
     border-radius: 8px;
     font-size: 18px;
     padding: 24px;
-  }  
+  }   */
 
   form {
     width: 100%;
@@ -37,20 +37,25 @@ div.input-container {
     display: flex;
 
     input {
+        background-color: #7e593b;
       color: #000;
       width: 100%;
       height: 45px;
       border-radius: 8px;
-      border: 1px solid #d9dde1;
+      border: 2px solid #000;
       font-size: 14px;
       padding: 24px;      
     }
-    Button {      
+    button {      
       border-radius: 8px;
+      margin-top: 24px;
       cursor: pointer;
       font-size: 18px;
       align-items: center;
       display: flex;
+      color: #fff;
+      height: 45px;
+      background-color: #050504;
       justify-content: center;
       height: 45px;
       width: 100%;
@@ -69,4 +74,24 @@ div.input-container {
 @media(max-width: 768px) {
     padding: 0;
   }
+`
+
+export const Results = styled.div`
+    width: 100%;
+    border: 2px solid #000;
+    border-radius: 8px;
+    padding: 5px;
+    margin: 5px;
+    text-align: justify;
+    text-transform: capitalize;
+    span {
+        display: flex;
+        h5 {
+            padding-right: 8px;
+        }
+        h6 {
+            font-weight: 500;
+        }
+        
+    }
 `
