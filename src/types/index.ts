0816b935c7ed70f98dfad4b5bed554aadc47ba8f
Caio Braga <caioaugustobrg@gmail.com>
2023-12-01 03:35:03 -0300

    :sparkles: feat: types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type FormData = {
+    autores?: string;
+      //anoPub?: string;
+      capitania?: string;
+     // fonte?: string;
+      lugares?: string;
+      nomes?: string;
+      temas?: string;
+    //  tematicas?: string;
+    temaPercent?: string
+    link?: string
+      titulo?: string;
+    }
