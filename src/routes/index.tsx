b575b38fe3896e17de50e49aecfe94f49d497300
Caio Braga <caioaugustobrg@gmail.com>
2023-12-17 02:37:23 -0300

    :sparkles: feat: new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,17 +5,21 @@ import {
 	Routes,
 	Navigate
 } from 'react-router-dom';
-import Home from '../pages/home';
+//import Home from '../pages/home';
 import ResultPage from '../pages/resultPage';
+import NewForm from '../pages/home/newform';
+import HelpPage from '../pages/HelpPage';
 
 
 const AppRoutes = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path='/' element={<Home />} />
+				<Route path='/' element={<NewForm />} />
 				<Route path="/search" element={ <ResultPage /> } />
+				<Route path="/help/search" element={ <HelpPage /> }/>
 				<Route path="*" element={<Navigate replace to="/"/>} />
+
 				
 			</Routes>
 		</Router>
